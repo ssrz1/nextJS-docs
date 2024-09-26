076b57bdf6f68a4abaa872d807402052951e00e8
ssrz1 <sajjadraza00.com>
2024-09-26 17:26:42 +0500

    Git-Coomit-2

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -15,7 +15,7 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+   //await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -23,6 +23,7 @@ export async function fetchRevenue() {
 
     return data.rows;
   } catch (error) {
+    console.log(error, "ERRROO")
     console.error('Database Error:', error);
     throw new Error('Failed to fetch revenue data.');
   }
